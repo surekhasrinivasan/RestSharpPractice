8cbeab5185b9f04d42f590d4d3c5b3798dbbc216
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-23 07:49:09 -0500

    Content from the client url is printed on console in string, object and array format

--- RestSharpPractice/Program.cs
+++ RestSharpPractice/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace RestSharpPractice
 {
@@ -10,6 +14,26 @@ namespace RestSharpPractice
     {
         static void Main(string[] args)
         {
-        }
+            // Declaring the client 
+            var client = new RestClient("https://jsonplaceholder.typicode.com/");
+
+            // Declare the request to the endpoint
+            var request = new RestRequest("/users", Method.GET);
+
+            // execute the request
+            IRestResponse response = client.Execute(request);
+            var content = response.Content; // raw content as string
+
+            //Console.WriteLine(content);
+            var deserializedContent = JsonConvert.DeserializeObject(content); // returns as Object
+
+            Console.WriteLine(deserializedContent);
+
+            // Outputs JSON Array from Object
+            //var output = JArray.FromObject(deserializedContent);
+            JArray output = new JArray(deserializedContent);
+
+            Console.WriteLine(output);
+        }   
     }
 }
